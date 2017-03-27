34b811082eb35a7d488754238d778fe0cdeca11f
René Baudisch <rene.baudisch@axelspringer.de>
2017-03-27 16:11:30 +0200

    added missing (

--- src/creative/Clicktag.js
+++ src/creative/Clicktag.js
@@ -1,5 +1,5 @@
 /* init method to get URI params, where 'clicktag', 'target' and 'frameId' will be passed */
-var getUriParams = function() {
+var getUriParams = (function() {
 	var query_string = {}
 	var query = window.location.search.substring(1);
 	var parmsArray = query.split('&');
@@ -10,7 +10,7 @@ var getUriParams = function() {
 		if (val != '' && pair[0] != '') query_string[pair[0]] = val;
 	}
 	return query_string;
-}();
+})();
 
 /* one way of using the clicktag is to set the URI from SRC as href and also the target of an a-tag */
 document.getElementById('clicktag').setAttribute('href', getUriParams.clicktag).setAttribute('target', getUriParams.target);
